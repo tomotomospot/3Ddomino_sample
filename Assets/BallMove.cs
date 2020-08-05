310c57177e4668a4c0b01fbb17a310ad3169e3cd
tomotomospot <tomonori.kato@litalico.co.jp>
2020-08-05 13:38:29 +0900

    first commit

---
+++ Assets/BallMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0,0,5);
+    }
+}
